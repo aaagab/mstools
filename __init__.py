f39092932139c9b99759c20458657d0bd83fd101
gabaaa <not-set>
2020-11-12 16:30:57 -0600

    adding base files

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# authors: Gabriel Auger
+# name: mstools
+# licenses: MIT 
+__version__ = "0.0.0"
+
+# from .dev.mstools import mstools
+# from .gpkgs import message as msg
+# from .gpkgs.options import Options
